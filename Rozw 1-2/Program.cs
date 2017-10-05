ad5b8b5411e18e8e07422478088b3213bbbc076c
pjastr <24464854+pjastr@users.noreply.github.com>
2017-10-05 08:37:49 +0200

    Add files via upload

---
+++ Rozw 1-2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rozw_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int liczba1 = 32;
+            int liczba2 = -22;
+            Console.WriteLine(liczba1 + liczba2);
+            Console.ReadKey();
+        }
+    }
+}
